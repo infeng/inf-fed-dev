c059d7b119c2b732b4c5829a7200b4c4f489fad4
infeng <fzhihao@outlook.com>
2017-03-31 16:04:43 +0800

    react-full, react-admin: add @types/webpack-env to fix require.ensure is not defined

--- templates/react-admin/src/routes/system.tsx
+++ templates/react-admin/src/routes/system.tsx
@@ -6,7 +6,7 @@ const routes = (
     <Route
       path="editPassword"
       getComponents={(nextState, cb) => {
-        require.ensure([], (require) => {
+        require.ensure([], (require: any) => {
           let Component = require('../containers/system/EditPassword');
           cb(null, {
             [nextState.routes[2].path]: Component,
@@ -17,7 +17,7 @@ const routes = (
     <Route
       path="operatorList"
       getComponents={(nextState, cb) => {
-        require.ensure([], (require) => {
+        require.ensure([], (require: any) => {
           let Component = require('../containers/system/UserList');
           let EditUser = require('../containers/system/EditUser');
           cb(null, {
